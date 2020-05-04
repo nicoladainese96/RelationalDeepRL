56c34fd58e1967edbfab658c3256448b5b72c5a1
nicoladainese96 <dainesn1@cs-041.org.aalto.fi>
2020-05-04 10:00:13 +0300

    bayesian optim results

--- Utils/HP_tuning.py
+++ Utils/HP_tuning.py
@@ -16,11 +16,11 @@ class BayesHPTuning():
     def step(self):
         start = time.time()
         
-        HPs = self.sample_params()
+        HPs, HPs_probs = self.sample_params()
         print("\n"+"="*40)
         print("Configuration sampled: ")
         for p in HPs:
-            print('\t'+p+' : ',HPs[p])
+            print('\t'+p+' : ',HPs[p], ' - prob: %.2f'%HPs_probs[p])
         print("="*40)
         V, V_lambda = self.evaluate_params(HPs)
         self.update_params(V, V_lambda)
@@ -28,16 +28,18 @@ class BayesHPTuning():
         elapsed = time.time() - start
         
         # here can be implemented conditions for logging, saving and stopping the whole thing
-        self.print_step(HPs, V, V_lambda, elapsed)
+        self.print_step(HPs, HPs_probs, V, V_lambda, elapsed)
         
         return
     
     def sample_params(self):
         HPs = {}
+        HPs_probs = {}
         for param in self.params:
-            value = self.params[param].sample()
+            value, prob = self.params[param].sample()
             HPs[param] = value
-        return HPs
+            HPs_probs[param] = prob
+        return HPs, HPs_probs
     
     def evaluate_params(self, HPs):
         # both of shape (len(lambdas), n_epochs)
@@ -56,11 +58,11 @@ class BayesHPTuning():
             self.params[param].update_stat(V, V_lambda)
         return
     
-    def print_step(self, HPs, V, V_lambda, elapsed):
+    def print_step(self, HPs, HPs_probs, V, V_lambda, elapsed):
         print("\n"+"="*40)
         print("Configuration sampled: ")
         for p in HPs:
-            print('\t'+p+' : ',HPs[p])
+            print('\t'+p+' : ',HPs[p], ' - prob: %.2f'%HPs_probs[p])
         print("="*40)
         for i, l in enumerate(self.lambdas):
             print("lambda: %.4f - V: %4f"%(self.lambdas[i],V_lambda[i]))
@@ -68,10 +70,26 @@ class BayesHPTuning():
         print("Time elapsed: %.2f s"%(elapsed))
         return
     
+    def return_best_model(self):
+        HPs = {}
+        for param in self.params:
+            probs = self.params[param].get_updated_sampling_probs()
+            idx = np.argmax(probs)
+            v = self.params[param].values[idx]
+            HPs[param] = v
+        return HPs
+    
+    def return_param_probs(self, param_name):
+        values = self.params[param_name].values
+        probs = self.params[param_name].get_updated_sampling_probs()
+        return values, probs
+        
+            
 class BayesParam():
     def __init__(self, values, priors, prior_sample_weight):
         self.values = values
         self.priors = priors
+        self.prior_adv = self.compute_prior_adv()
         self.N = prior_sample_weight
         self.global_V = []
         self.last_sampled = None
@@ -83,8 +101,9 @@ class BayesParam():
         probs = self.get_updated_sampling_probs()
         idx = np.random.choice(np.arange(len(self.values)), p=probs)
         value = self.values[idx]
+        prob = probs[idx]
         self.last_sampled = idx
-        return value
+        return value, prob
         
     def get_updated_sampling_probs(self):
         expected_global_V = np.mean(self.global_V)
@@ -92,10 +111,10 @@ class BayesParam():
         for idx in self.stat:
             if self.stat[idx]['freq'] != 0:
                 expected_Vj = np.mean(self.stat[idx]['V'])
-                adv_j = expected_Vj - expected_global_V
+                adv_j = (expected_Vj - expected_global_V)/expected_global_V
             else:
                 adv_j = 0 # every value would do
-            biased_adv_j = (self.N*self.priors[idx]+self.stat[idx]['freq']*adv_j)/(self.N+self.stat[idx]['freq'])
+            biased_adv_j = (self.N*self.prior_adv[idx]+self.stat[idx]['freq']*adv_j)/(self.N+self.stat[idx]['freq'])
             advantages.append(biased_adv_j)
         # sampling probs are the softmax of the biased advantages 
         advantages = np.array(advantages)
@@ -109,3 +128,8 @@ class BayesParam():
         self.stat[idx]['freq'] += 1
         self.global_V.append(V)
         return
+    
+    def compute_prior_adv(self):
+        log_pi = np.log(self.priors)
+        prior_adv = log_pi - log_pi.mean()
+        return prior_adv
